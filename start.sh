a2a94dec9b98bf4ba7b28cb7d483386495d85e94
Ankit <ankitkumarbh@gmail.com>
2025-01-24 12:22:21 +0530

    first commit

---
+++ start.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+filepath="$(realpath $0)"
+mypath="$(dirname "$filepath")"
+cd $mypath
+GIT_EXEC_PATH=/app/.apt/usr/lib/git-core
+export GIT_EXEC_PATH=/app/.apt/usr/lib/git-core
+
+git clone $REPO_URL BOT
+# git clone $REPO_URL2 BOT2
+# bash BOT/start.sh & bash BOT2/start.sh & wait -n
+bash BOT/start.sh
